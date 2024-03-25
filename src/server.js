f4840b36c20e1ed6db33cea7b6d8f9c32e27d2a3
Victor Brown <sirbrv@gmail.com>
2024-03-24 21:22:41 -0300

    Se actualizan variables de entorno

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 require("./config.js");
 const port = process.env.PORT;
-console.log("puerto.....:", process.env.SMPT_HOST);
+const host = process.env.HOST;
 const app = express();
 
 app.use(morgan("dev"));
@@ -11,19 +11,8 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
-// const enviarMail = require("./services/sendMail");
-
-// console.log("Voy a enviar mensaje..:");
-// enviarMail({
-//   email: "sirbrv@gmail.com",
-//   subject: "Compra de Boletos",
-//   message: `Hola Victor Brown, activa tu cuenta en el enlace siguiente.: `,
-// });
-
-// console.log(" mensaje enviado..:");
-
-app.get("/", function (req, res) {
-  res.send("Proyecto de la Académia IT");
+app.get("/", (req, res) => {
+  res.status(200).sendFile(__dirname + "/public/html/welcome.html");
 });
 
 app.use("/api", require("./routes/VerifyRoutes.js"));
@@ -33,6 +22,10 @@ app.use("/api", require("./routes/EventsRoutes.js"));
 app.use("/api", require("./routes/AcademyRoutes.js"));
 app.use("/api", require("./routes/TicketsRoutes.js"));
 
+app.use((red, res, next) => {
+  res.status(404).sendFile(__dirname + "/public/html/404.html");
+});
+
 app.listen(port, () => {
-  console.log("Servidor disponible en  http://localhost:" + port);
+  console.log(`Servidor disponible en  ${host}:${port}`);
 });
